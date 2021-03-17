aebea04d9cfbaf87ab2a9d7cd93480635f604646
Matthias Fuchs <matz.fuchs@gmx.at>
2021-03-17 17:07:40 +0100

    Support conan/docker credentials

--- app/models/profile.js
+++ app/models/profile.js
@@ -3,6 +3,9 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 export default class ProfileModel extends Model {
     @attr name;
     @attr container;
+    @attr docker_registry;
+    @attr docker_user;
+    @attr docker_password;
     @attr settings;
     @belongsTo('ecosystem') ecosystem;
 }
