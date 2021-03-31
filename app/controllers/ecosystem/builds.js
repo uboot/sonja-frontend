da3479bc1778d496399ac90b112a50a575577f0d
Matthias Fuchs <matz.fuchs@gmx.at>
2021-03-31 22:11:13 +0200

    Add button to (re-)start builds

--- app/controllers/ecosystem/builds.js
+++ app/controllers/ecosystem/builds.js
@@ -4,6 +4,11 @@ import { action } from '@ember/object';
 export default class EcosystemBuildsController extends Controller {
 
   @action
+  startBuild(model) {
+    model.status = 'new';
+    model.save();
+  }
+  @action
   stopBuild(model) {
     model.status = 'stopping';
     model.save();
