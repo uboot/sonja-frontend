d7d74e1fa7d584616d1900ee836ab482890055b9
Matthias Fuchs <matz.fuchs@gmx.at>
2021-04-19 22:18:03 +0200

    Display commit author

--- app/models/commit.js
+++ app/models/commit.js
@@ -3,6 +3,8 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 export default class CommitModel extends Model {
   @attr sha;
   @attr message;
+  @attr user_name;
+  @attr user_email;
   @belongsTo('repo') repo;
   @belongsTo('channel') channel;
   @hasMany('build') builds;
