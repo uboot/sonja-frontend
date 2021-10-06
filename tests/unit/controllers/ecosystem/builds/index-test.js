80cb16a5d2e8b731c51dd846fe1be001b39bb900
Matthias Fuchs <matthias1.fuchs@zeiss.com>
2021-10-06 11:48:47 +0200

    Upgrade to ember 3.28

--- tests/unit/controllers/ecosystem/builds/index-test.js
+++ tests/unit/controllers/ecosystem/builds/index-test.js
@@ -6,7 +6,7 @@ module('Unit | Controller | ecosystem/builds', function(hooks) {
 
   // TODO: Replace this with your real tests.
   test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:ecosystem/builds');
+    let controller = this.owner.lookup('controller:ecosystem/builds/index');
     assert.ok(controller);
   });
 });
