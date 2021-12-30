19192e3df69bbfa7bd3fb22d32c6e08bcf41c400
Matthias Fuchs <matz.fuchs@gmx.at>
2021-12-30 21:55:41 +0100

    Fix serialization of empty relationships

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,3 +1,19 @@
 import JSONAPISerializer from '@ember-data/serializer/json-api';
 
-export default class ApplicationSerializer extends JSONAPISerializer {}
+export default class ApplicationSerializer extends JSONAPISerializer {
+  
+  serializeBelongsTo(snapshot, json, relationship) {
+    let key = relationship.key;
+    let belongsToId = snapshot.belongsTo(key, { id: true });
+
+    // Do not serialize the relationship if it points to an object with id ==
+    // null. The API backend expects a string id and throws an error if it
+    // receives null.
+    if (belongsToId === null) {
+      return;
+    }
+
+    super.serializeBelongsTo(snapshot, json, relationship)
+  }
+
+}
