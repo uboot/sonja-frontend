0404a5d89f1bb7e59a26090af26c2b20f0b369b7
Matthias Fuchs <matz.fuchs@gmx.at>
2022-02-01 17:00:33 +0100

    Fix array members of new objects

--- app/controllers/new.js
+++ app/controllers/new.js
@@ -11,6 +11,7 @@ export default class NewController extends Controller {
   async createEcosystem(model) {
     let ecosystem = this.store.createRecord('ecosystem', {
       name: model.name,
+      credentials: [],
     });
     await ecosystem.save();
     this.router.transitionTo('ecosystem.settings', ecosystem);
