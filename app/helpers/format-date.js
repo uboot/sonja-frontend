e74d5bde3251dc2fa4618f7620773aecbff267e2
Matthias Fuchs <matz.fuchs@gmx.at>
2022-04-25 17:31:10 +0200

    Add creation time to build model

---
+++ app/helpers/format-date.js
@@ -0,0 +1,12 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(function formatDate([date]) {
+  let day = date.getDate().toString().padStart(2, '0');
+  let month = (date.getMonth() + 1).toString().padStart(2, '0');
+  let year = date.getFullYear();
+  let hours = date.getHours().toString().padStart(2, '0');
+  let minutes = date.getMinutes().toString().padStart(2, '0');
+  let seconds = date.getSeconds().toString().padStart(2, '0');
+  
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+});
