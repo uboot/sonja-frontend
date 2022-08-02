27e7bb18403e0b4aeca24d912eced101dc4d6562
Matthias Fuchs <matz.fuchs@gmx.at>
2022-08-02 22:19:02 +0200

    Support requirement relations in models

--- app/models/package.js
+++ app/models/package.js
@@ -3,6 +3,7 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 export default class PackageModel extends Model {
   @attr package_id;
   @belongsTo('recipe-revision') recipe_revision;
+  @hasMany('build') builds;
   @hasMany('package', { inverse: 'required_by' }) requires;
   @hasMany('package', { inverse: 'requires' }) required_by;
 }
