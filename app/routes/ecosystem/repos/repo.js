3182e732995f754be3ca71c7868fbe16c9b25c37
Matthias Fuchs <matz.fuchs@gmx.at>
2022-12-08 15:01:22 +0100

    Add event service

--- app/routes/ecosystem/repos/repo.js
+++ app/routes/ecosystem/repos/repo.js
@@ -1,10 +1,10 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import ENV from 'sonja/config/environment';
 
 export default class EcosystemReposRepoRoute extends Route {
   @service store;
   @service infinity;
+  @service sonjaEvents;
 
   model(params) {
     let ecosystem = this.modelFor('ecosystem');
@@ -21,29 +21,26 @@ export default class EcosystemReposRepoRoute extends Route {
     if (params.profile) {
       dataParams.profile_id = params.profile;
     }
+
+    let self = this;
+    this.sonjaEvents.addObserver('payload', async () => {
+      let data = self.sonjaEvents.payload.data;
+      
+      if (data.type != "builds")
+      {
+        return;
+      }
+
+      let record = await self.store.findRecord('build', data.id);
+      let model = await self.modelFor('ecosystem.repos.repo');
+      if (!model.builds.isAny('id', record.id)) {
+        self.infinity.pushObjects(model.builds.content, [record]);
+      }
+    });
     
     return { 
       builds: this.infinity.model('build', dataParams),
       repo: this.store.findRecord('repo', params.repo_id)
     }
-  }
-
-  activate() {
-    let repoId = this.paramsFor('ecosystem.repos.repo').repo_id;
-    this.eventSource = new EventSource(`/${ENV.apiNamespace}/event/repo/${repoId}/build`);
-    let self = this;
-    this.eventSource.addEventListener('update', async function(e) {
-      let data = JSON.parse(e.data);
-      self.store.pushPayload(data);
-      let record = await self.store.findRecord('build', data.data.id);
-      let model = await self.modelFor('ecosystem.repos.repo');
-      if (!model.builds.isAny('id', record.id)){
-        self.infinity.pushObjects(model.builds.content, [record]);
-      }
-    });
-  }
-
-  deactivate() {
-    this.eventSource?.close();
   }
 }
