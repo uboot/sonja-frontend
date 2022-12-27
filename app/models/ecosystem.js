f2539ade463e5da69aeeecd5a79060f38e25c3ac
Matthias Fuchs <matz.fuchs@gmx.at>
2022-12-27 10:38:35 +0100

    Move general settings to configuration

--- app/models/ecosystem.js
+++ app/models/ecosystem.js
@@ -9,10 +9,6 @@ export default class EcosystemModel extends Model {
   @attr conan_config_branch;
   @attr conan_user;
   @attr conan_password;
-  @attr public_ssh_key;
-  @attr known_hosts;
-  @attr git_credentials;
-  @attr docker_credentials;
   @hasMany('repo', { async: true, inverse: 'ecosystem' }) repos;
   @hasMany('profile', { async: true, inverse: 'ecosystem' }) profiles;
   @hasMany('channel', { async: true, inverse: 'ecosystem' }) channels;
