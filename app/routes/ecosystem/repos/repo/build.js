98fd28b6498ffce388d6600517492be93170b4b0
Matthias Fuchs <matz.fuchs@gmx.at>
2023-01-12 14:45:46 +0100

    Fix events

--- app/routes/ecosystem/repos/repo/build.js
+++ app/routes/ecosystem/repos/repo/build.js
@@ -1,8 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import ENV from 'sonja/config/environment';
 
 export default class EcosystemReposRepoBuildRoute extends Route {
   @service store;
-  @service sonjaEvents;
 }
